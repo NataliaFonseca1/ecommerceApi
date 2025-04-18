f321ddb5289ed6a393e36c8223563bc40f767715
NataliaFonseca1 <fonsecanatalianf@gmail.com>
2025-04-18 13:05:40 -0300

    endpoints for products using feingClient

--- src/main/java/dev/java/ecommerce/apiQuickShop/Controller/ProductController.java
+++ src/main/java/dev/java/ecommerce/apiQuickShop/Controller/ProductController.java
@@ -1,5 +1,6 @@
 package dev.java.ecommerce.apiQuickShop.Controller;
 
+import dev.java.ecommerce.apiQuickShop.Client.PlatziProductResponse;
 import dev.java.ecommerce.apiQuickShop.Service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -8,21 +9,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/products")
-@RequiredArgsConstructor
 public class ProductController {
 
-private ProductService productService;
+private  ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @GetMapping
-public ResponseEntity<Void> getAllProducts(){
-    return ResponseEntity.ok().build();
+public ResponseEntity<List<PlatziProductResponse>> getAllProducts(){
+    return ResponseEntity.ok(productService.getAllProducts());
 }
 
 @GetMapping("/{id}")
-    public ResponseEntity<Void> getProductById(@PathVariable Long id) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<PlatziProductResponse> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
 }
 
 
--- src/main/java/dev/java/ecommerce/apiQuickShop/Service/ProductService.java
+++ src/main/java/dev/java/ecommerce/apiQuickShop/Service/ProductService.java
@@ -1,15 +1,28 @@
 package dev.java.ecommerce.apiQuickShop.Service;
 
+import dev.java.ecommerce.apiQuickShop.Client.PlatziProductResponse;
+import dev.java.ecommerce.apiQuickShop.Client.PlatziStoreClient;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
 
+    private final PlatziStoreClient platziStoreClient;
+
+    public ProductService(PlatziStoreClient platziStoreClient) {
+        this.platziStoreClient = platziStoreClient;
+    }
 
 
-public void getAllProducts(){}
+    public List<PlatziProductResponse> getAllProducts(){
+    return platziStoreClient.getAllProducts();
+}
 
-public void getProductById(Long id){}
+public PlatziProductResponse getProductById(Long id){
+    return platziStoreClient.getProductById(id);
+}
 
 
 }
